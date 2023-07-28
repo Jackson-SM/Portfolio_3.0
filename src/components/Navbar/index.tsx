263c581fbddc8f93e20eab0fce2275db84f20db7
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-07-28 17:24:23 -0300

    feat: add navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import * as Styled from "./styles";
+import { Logo } from "../Logo";
+
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Styled.Navbar className={`${poppins.className}`}>
+      <Logo />
+    </Styled.Navbar>
+  );
 };
--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -1,7 +1,20 @@
 import { styled } from "@/config/stitches.config";
 
-export const StyledNavbar = styled("nav", {
+export const Navbar = styled("nav", {
   height: "80px",
 
-  background: "$backgroundLight",
+  background: "$backgroundTransparence2",
+
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+
+  fontWeight: "bold",
+
+  padding: "5px",
+
+  position: "fixed",
+  backdropFilter: "blur(5px)",
+  width: "100%",
+  borderBottom: "1px solid $backgroundLight2",
 });
