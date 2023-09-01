09ce64c298564ce2cfe052a13327b13df530aab0
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-09-01 12:51:23 -0300

    fix: fixed font in document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { getCssText } from "@/config/stitches.config";
 import { Html, Head, Main, NextScript } from "next/document";
-import { Poppins } from "next/font/google";
-
-const poppins = Poppins({ weight: "400", subsets: ["latin"] });
-
 export default function Document() {
   return (
     <Html lang="en">
@@ -14,7 +10,7 @@ export default function Document() {
           dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
       </Head>
-      <body className={poppins.className}>
+      <body>
         <Main />
         <NextScript />
       </body>
