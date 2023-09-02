4b734deaeb76fa8093ae23160699d507ee7747fb
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-09-02 20:29:04 -0300

    feat: add link component

--- src/components/Link/styles.ts
+++ src/components/Link/styles.ts
@@ -2,5 +2,14 @@ import { styled } from "@/config/stitches.config";
 import Linked from "next/link";
 
 export const Link = styled(Linked, {
-  color: "$primary",
+  color: "$text",
+  fontSize: "0.85rem",
+
+  textDecoration: "none",
+
+  transition: "all ease 180ms",
+
+  "&:hover": {
+    color: "$primary",
+  },
 });
--- src/components/Links/styles.ts
+++ src/components/Links/styles.ts
@@ -2,4 +2,11 @@ import { styled } from "@/config/stitches.config";
 
 export const Link = styled("a", {
   color: "$text",
+  textDecoration: "none",
+
+  transition: "all ease 150ms",
+
+  "&:hover": {
+    color: "$primary",
+  },
 });
