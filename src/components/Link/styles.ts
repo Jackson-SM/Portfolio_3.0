da6f0f7146ba95553ce7d2b8bc4e36dc4459294e
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2024-01-24 15:38:37 -0300

    feat: add svg support in link

--- src/components/Link/styles.ts
+++ src/components/Link/styles.ts
@@ -4,6 +4,9 @@ import Linked from "next/link";
 export const Link = styled(Linked, {
   fontSize: "0.85rem",
 
+  display: "flex",
+  alignItems: "center",
+
   color: "$text",
 
   textDecoration: "none",
@@ -12,5 +15,39 @@ export const Link = styled(Linked, {
 
   "&:hover": {
     color: "$primary",
+
+    "& > svg": {
+      color: "$primary",
+    },
+  },
+
+  "& > svg": {
+    color: "$textDark",
+    width: 25,
+    height: 25,
+    transition: "all ease 180ms",
+  },
+
+  variants: {
+    size: {
+      "1": {
+        fontSize: "$1",
+      },
+      "2": {
+        fontSize: "$2",
+      },
+      "3": {
+        fontSize: "$3",
+      },
+      "4": {
+        fontSize: "$4",
+      },
+      "5": {
+        fontSize: "$5",
+      },
+      "6": {
+        fontSize: "$6",
+      },
+    },
   },
 });
