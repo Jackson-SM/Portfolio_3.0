c4e2fc6d3be25a0134cdcadc63397755fa7f9245
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2024-01-24 15:40:05 -0300

    feat: add circle and gradient at profile picture in section hero

--- src/components/pages/Home/SectionHero/ProfilePicture/styles.ts
+++ src/components/pages/Home/SectionHero/ProfilePicture/styles.ts
@@ -5,6 +5,12 @@ export const ProfilePictureBox = styled(Box, {
   width: "500px",
   minWidth: "30%",
 
+  position: "relative",
+  borderRadius: "50%",
+  boxShadow: "0 0 10px 0 $colors$primaryDark",
+
+  backgroundImage: `radial-gradient(circle at 0% 10%,$primaryTransparence 1%, $background 80%)`,
+
   "@large": {
     width: "50%",
   },
