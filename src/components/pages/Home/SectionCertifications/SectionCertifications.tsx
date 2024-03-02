5688a7b2a8b0ffe1fc4f707d4188aa27d86bacab
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2024-03-02 19:09:12 -0300

    feat: add certifications button

--- src/components/pages/Home/SectionCertifications/SectionCertifications.tsx
+++ src/components/pages/Home/SectionCertifications/SectionCertifications.tsx
@@ -1,10 +1,25 @@
 import { HeadingTitle } from "@/components/HeadingTitle/Heading";
+import { Box, Grid } from "@radix-ui/themes";
+import { ButtonCertification } from "./components/ButtonCertification/ButtonCertification";
 import * as Styles from "./styles";
 
 export const SectionCertifications = () => {
   return (
     <Styles.SectionCertifications>
       <HeadingTitle>Certifications</HeadingTitle>
+      <Grid columns="auto" gap="3" width="auto">
+        <Box>
+          <ButtonCertification asChild>
+            <a href="#cert1">Certificado 1</a>
+          </ButtonCertification>
+        </Box>
+        <Box>
+          <ButtonCertification>Certificado 2</ButtonCertification>
+        </Box>
+        <Box>
+          <ButtonCertification>Certificado 3</ButtonCertification>
+        </Box>
+      </Grid>
     </Styles.SectionCertifications>
   );
 };
